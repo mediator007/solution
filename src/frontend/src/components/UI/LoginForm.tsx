7bef63b9734c97d019c6fe7add403f9e8b8dc44e
mediator007 <mediator007@mail.ru>
2023-04-25 21:45:07 +0300

    login form component

--- src/frontend/src/components/UI/LoginForm.tsx
+++ src/frontend/src/components/UI/LoginForm.tsx
@@ -1,28 +1,43 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-function LoginForm() {
-  return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-        </Form.Text>
-      </Form.Group>
+const LoginForm = () => {
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-  );
-}
+  function login (event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    console.log('LOGIN!')
+  };
+  
+  function LoginForm() {
+    return (
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={login}>
+          Войти
+        </Button>
+      </Form>
+    )
+  };
+
+  return(
+    <div>
+      {LoginForm()}
+    </div>
+  )
+};
+
+
 
 export default LoginForm;
--- src/frontend/src/pages/Login.tsx
+++ src/frontend/src/pages/Login.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Grid, GridItem } from '@consta/uikit/Grid'; 
+import LoginForm from "../components/UI/LoginForm";
 
 const Login = () => {
     return(
         <div>
-            Login
+            <Grid style={{marginTop: '10%'}} xAlign="center" cols='3'>
+                <GridItem col='1'/>
+                <GridItem col='1'>
+                    <LoginForm/>
+                </GridItem>
+            </Grid>
         </div>
     )
 };
